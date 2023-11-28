f8b071a23019063d8f966e2fa60e66e0a5797a7f
Osamu Takeuchi <osamu@big.jp>
2023-11-28 23:12:13 +0900

    ログインしているかどうかでアクセス制限を掛けた

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,19 @@
 import { auth } from '$lib/server/lucia';
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
+import { path } from '$lib';
 
 const authHandler: Handle = async ({ event, resolve }) => {
   event.locals.auth = auth.handleRequest(event);
   event.locals.session = await event.locals.auth.validate();
+
+  if (event.route.id?.match(/\/\(login\)\//) && !event.locals.session?.user) {
+    throw redirect(302, path('/session/new'));
+  }
+
+  if (event.route.id?.match(/\/\(logout\)\//) && event.locals.session?.user) {
+    throw redirect(302, path('/'));
+  }
 
   return await resolve(event);
 };
