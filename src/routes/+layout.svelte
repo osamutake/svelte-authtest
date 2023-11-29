cd6fe78ac5d5b56bb348c3e1fd3a05977c4457d7
Osamu Takeuchi <osamu@big.jp>
2023-11-29 09:46:45 +0900

    flash メッセージを toast するようにした

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
   import { getFlash } from 'sveltekit-flash-message';
   import { page } from '$app/stores';
+  import toast, { Toaster } from 'svelte-french-toast';
 
   const flash = getFlash(page);
+
+  flash.subscribe(($flash) => {
+    if (!$flash) return;
+
+    toast($flash.message, {
+      icon: $flash.type == 'success' ? '✅' : '❌',
+    });
+
+    // Clearing the flash message could sometimes
+    // be required here to avoid double-toasting.
+    flash.set(undefined);
+  });
 </script>
 
 {#if $flash}
@@ -10,4 +23,5 @@
   <div style:background-color={bg} class="flash">{$flash.message}</div>
 {/if}
 
+<Toaster />
 <slot />
